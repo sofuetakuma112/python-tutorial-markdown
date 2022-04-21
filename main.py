3176ad348ca57353ae65259e41722049855545ff
sofuetakuma <kawahagi0620@gmail.com>
2022-04-21 23:43:01 +0900

    関数の章まで一通り書いた, 例外処理は削除した

---
+++ main.py
@@ -0,0 +1,18 @@
+print("python実習")
+print('Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+print('left' + ' ' + "right")
+
+print(int(1) + int(3)) # 足し算
+print(int(1) - int(3)) # 引き算
+print(int(1) * int(3)) # かけ算
+print(int(1) / int(3)) # 割り算
+print(int(1) % int(3)) # 余り
+print(int(2) ** int(3)) # べき乗
+
+a = 5
+b = 10
+
+def multiply(x, y):
+  result = x * y
+
+print(multiply(a, b))
